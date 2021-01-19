107a5a88e88471fc441aea5068228fb605accf92
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-01-19 11:02:15 -0800

    add data to array during array creation
    Co-authored-by: Mike Pingel <mdpingel@gmail.com>
    Co-authored-by: Sage Sypert <sage.sypert@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
   $("form#favorite").submit(function(event) {
     event.preventDefault();
     
-    const favoriteThings = [];
-    favoriteThings.push($("#favorite-thing1").val());
-    favoriteThings.push($("#favorite-thing2").val());
-    favoriteThings.push($("#favorite-thing3").val());
-    favoriteThings.push($("#favorite-thing4").val());
+    const favoriteThings = [$("#favorite-thing1").val(), $("#favorite-thing2").val(), $("#favorite-thing3").val(), $("#favorite-thing4").val()];
+    // favoriteThings.push($("#favorite-thing1").val());
+    // favoriteThings.push($("#favorite-thing2").val());
+    // favoriteThings.push($("#favorite-thing3").val());
+    // favoriteThings.push($("#favorite-thing4").val());
     console.log(favoriteThings);
   });
 });
